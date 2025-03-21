692645961a9ad49c67658884f6eb52dcdc91ca45
Yegor Vedernikov <vedernikov.yegor@gmail.com>
2025-03-21 17:49:50 +0200

    Disable linter

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
   // experimental: {
   //   ppr: 'incremental'
   // }
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
